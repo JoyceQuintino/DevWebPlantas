61c0794b35eb5b99a21c445d5cbb97069f63f83e
Joyce Quintino <joycequintino11@gmail.com>
2019-02-10 00:44:46 -0200

    estudos-vue.js parte 2

---
+++ js/p1.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+    el: "#app",
+    data: {
+        titulo: "Aula 1 - Vuejs"
+    }
+});
+var app2 = new Vue({
+    el: '#app2',
+    data: {
+        message: 'You loaded this page on ' + new Date().toLocaleString()
+    }
+});
