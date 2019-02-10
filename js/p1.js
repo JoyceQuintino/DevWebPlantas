d826bddd874644a06165f8069e95fc6065bf6bd5
Joyce Quintino <joycequintino11@gmail.com>
2019-02-10 01:51:51 -0200

    integração com o bootstrap

--- js/p1.js
+++ js/p1.js
@@ -4,9 +4,12 @@ var app = new Vue({
         titulo: "Aula 1 - Vuejs"
     }
 });
+
 var app2 = new Vue({
     el: '#app2',
     data: {
         message: 'You loaded this page on ' + new Date().toLocaleString()
     }
 });
+
+
