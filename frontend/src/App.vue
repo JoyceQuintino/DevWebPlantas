cae478b55a04738275ab8d6af3a9d8b6ecb9e451
Joyce Quintino <joycequintino11@gmail.com>
2019-03-18 15:13:34 -0300

    Atualização do projeto com vue-cli

---
+++ frontend/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <nav class="navbar navbar-expand-md bg-dark navbar-dark">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">PlantLar</a>
+            </li>
+        </ul>
+    </nav>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
