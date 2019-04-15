9a711eef5b533695af016a26566f94e72be5d317
Joyce Quintino <joycequintino11@gmail.com>
2019-04-14 22:33:00 -0300

    criação do componente sidebar

---
+++ ZeDeWilliam/sorveteria/src/components/Mysidebar.vue
@@ -0,0 +1,24 @@
+<template>
+  <Mysidebar :menu="menu"/>
+</template>
+
+<script>
+    export default {
+        data() {
+            menu: [
+                {
+                    href: '/',
+                    title: 'Menu de navegação'
+                },
+                {
+                    href: '/',
+                    title: 'Dashboard'
+                },
+                {
+                    href: '#',
+                    title: 'Cadastro de remessa'
+                },
+            ]
+        }
+    }
+</script>
