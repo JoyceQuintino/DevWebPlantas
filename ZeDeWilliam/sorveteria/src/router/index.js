c8c9757698908e8507e2a26c992538506fbe19e2
Joyce Quintino <joycequintino11@gmail.com>
2019-04-14 21:33:25 -0300

    adição das demais rotas

--- ZeDeWilliam/sorveteria/src/router/index.js
+++ ZeDeWilliam/sorveteria/src/router/index.js
@@ -1,6 +1,10 @@
+/* eslint-disable */
 import Vue from 'vue';
 import Router from 'vue-router';
-import Mydashboard from '@/components/Mydashboard'
+import Mydashboard from '@/components/Mydashboard';
+import Mysidebar from '@/components/Mysidebar';
+import Mylogin from '@/components/Mylogin';
+import Mymodal from '@/components/Mymodal';
 
 Vue.use(Router);
 
@@ -14,6 +18,21 @@ export default new Router({
       path: '/dashboard',
       name: 'dashboard',
       component: Mydashboard
+    },
+    {
+      path: '/sidebar',
+      name: 'sidebar',
+      component: Mysidebar
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Mylogin
+    },
+    {
+      path: '/modal',
+      name: 'modal',
+      component: Mymodal
     }
   ],
 });
